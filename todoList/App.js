b40c17f08b3889eba7ac0e08f17257854aea6123
steakncheese <romell.bermundo@outlook.com>
2023-01-06 11:19:08 -0700

    feat: create task component

--- todoList/App.js
+++ todoList/App.js
@@ -1,5 +1,6 @@
-import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Task from "./components/Task";
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Today's tasks</Text>
         <View style={styles.items}></View>
+        <Task />
       </View>
     </View>
   );
