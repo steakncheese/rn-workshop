0b310f53fc9c21c1e3d8a82199f293fed46a7b0e
steakncheese <romell.bermundo@outlook.com>
2023-01-27 20:16:09 -0700

    feat: test tab nagivator

--- todoList/App.js
+++ todoList/App.js
@@ -5,16 +5,70 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import Detail from "./src/screens/Detail";
+import PantryNav from "./src/screens/MyPantry/_PantryNav";
+import PlanNav from "./src/screens/PlanDinner/_PlanNav";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Detail" component={Detail} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        initialRouteName="Recipe Nav"
+        options={{ headerShown: false }}
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Recipe") {
+              iconName = focused ? "restaurant" : "restaurant";
+            } else if (route.name === "Account") {
+              iconName = focused ? "settings" : "settings-outline";
+            } else if (route.name === "Plan Meal") {
+              iconName = focused ? "calendar" : "calendar";
+            } else if (route.name === "Pantry") {
+              iconName = focused ? "fast-food" : "fast-food";
+            } else if (route.name === "Home") {
+              iconName = focused ? "home" : "home";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={30} color={color} />;
+          },
+          tabBarActiveTintColor: "#D3FAD9",
+          tabBarInactiveTintColor: "#47c053",
+          tabBarStyle: { backgroundColor: "#333333", height: "8%" },
+          tabBarLabelStyle: { fontSize: 12, height: 25, marginTop: -10 },
+        })}
+      >
+        <Tab.Screen name="Account">
+          {(props) => <AccountNav {...props} />}
+        </Tab.Screen>
+        <Tab.Screen name="Plan Meal">
+          {(props) => <PlanNav {...props} recipeList={selectedRecipesList} />}
+        </Tab.Screen>
+        <Tab.Screen name="Home">{(props) => <Home {...props} />}</Tab.Screen>
+        <Tab.Screen name="Recipe">
+          {(props) => (
+            <RecipeNav
+              {...props}
+              ingredientList={ingredientList}
+              selectedRecipesList={selectedRecipesList}
+              setSelectedRecipesList={setSelectedRecipesList}
+            />
+          )}
+        </Tab.Screen>
+        <Tab.Screen name="Pantry">
+          {(props) => (
+            <PantryNav
+              {...props}
+              ingredientList={ingredientList}
+              setIngredientList={setIngredientList}
+            />
+          )}
+        </Tab.Screen>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
